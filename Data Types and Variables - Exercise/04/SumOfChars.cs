11437a2d6e30caf78019cb673e87777de07da163
BuildTools <unconfigured@null.spigotmc.org>
2019-02-22 15:57:34 +0200

    Data Types and Variables - Exercise

---
+++ Data Types and Variables - Exercise/04/SumOfChars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04
+{
+    class SumOfChars
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                char nextNum = char.Parse(Console.ReadLine());
+                sum += (int)nextNum;
+            }
+
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
