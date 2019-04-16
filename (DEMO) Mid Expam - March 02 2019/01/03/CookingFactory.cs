89169b3d3d7a204639a2cb54b43698017e7817a5
BuildTools <unconfigured@null.spigotmc.org>
2019-04-16 17:51:26 +0300

    Next sync

---
+++ (DEMO) Mid Expam - March 02 2019/01/03/CookingFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03
+{
+    class CookingFactory
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            List<int> numbers = new List<int>();
+            List<int> sum = new List<int>();
+            List<string> inputsAfter = new List<string>();
+            List<int> averange = new List<int>();
+
+
+            while (command != "Bake it!")
+            {
+                List<int> input = command
+                    .Split('#')
+                    .Select(int.Parse)
+                    .ToList();
+
+                inputsAfter.Add(input.ToString());
+
+                for (int j = 0; j < input.Count; j++)
+                {
+                    numbers.Add(input[j]);
+                }
+
+                int lastSum = 0;
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    lastSum += numbers[i];
+                }
+
+                sum.Add(lastSum);
+                averange.Add(numbers.Count / lastSum);
+
+                command = Console.ReadLine();
+            }
+
+            
+
+            sum.Sort();
+            averange.Sort();
+
+           
+        }
+        
+    }
+}
---
+++ Lists - Exercise/Lists - Exercise/01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 32 54 21 12 4 0 23 75
+
+            List<int> input = Console.ReadLine()
+                        .Split()
+                        .Select(int.Parse)
+                        .ToList();
+
+
+
+
+
+
+
+
+        }
+    }
+}
