22896f09ff3b8af2569c8d5ee053c47f17fe0aa4
BuildTools <unconfigured@null.spigotmc.org>
2019-12-07 18:37:59 +0200

    COmmit

--- TechModuleProjects/Arrays - Exercise/Arrays - Exercise/07/MaxSequenceOfEqualElements.cs
+++ TechModuleProjects/Arrays - Exercise/Arrays - Exercise/07/MaxSequenceOfEqualElements.cs
@@ -11,22 +11,37 @@ namespace _07
         {
             List<int> input = Console.ReadLine()
                 .Split()
-                .Select(int.Parse)
+                .Select(x => int.Parse(x))
                 .ToList();
 
+            List<int> numbers = GetNumbersFromInput(input);
 
+            Console.WriteLine(string.Join(' ', numbers));
         }
 
-        private static List<int> GetLength(List<int> a)
+        private static List<int> GetNumbersFromInput(List<int> input)
         {
-            int counter = 0;
+            Dictionary<int, List<int>> numbers = new Dictionary<int, List<int>>();
+            int indexCounterOfDict = 0;
+            int lastIndex = input.Count - 1;
+            List<int> secondNum = new List<int>();
 
-            List<int> numbers = new List<int>();
-
-            for (int i = 0; i < a.Count; i++)
+            for (int i = 0; i < input.Count; i++)
             {
+                if (input[i] == input[i + 1] && i != lastIndex)
+                {
+                    for (int j = 0; j < input.Count - i; j++)
+                    {
+                        if (input[j] == input[j + 1] && j != lastIndex)
+                        {
+                            secondNum.Add(input[j]);
 
+                        }
+                    }
+                }
             }
+
+
         }
     }
 }
