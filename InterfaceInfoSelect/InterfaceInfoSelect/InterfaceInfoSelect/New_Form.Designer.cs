f95d59371544bdc4b5d44e8aa237625f294b9ac7
Nikolai <xxthemineproxx@gmail.com>
2019-12-22 13:28:05 +0200

    Interface

---
+++ InterfaceInfoSelect/InterfaceInfoSelect/InterfaceInfoSelect/New_Form.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace InterfaceInfoSelect
+{
+    partial class New_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(New_Form));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ID_Append = new System.Windows.Forms.TextBox();
+            this.Amount_Append = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Amount";
+            // 
+            // ID_Append
+            // 
+            this.ID_Append.Location = new System.Drawing.Point(93, 28);
+            this.ID_Append.Name = "ID_Append";
+            this.ID_Append.Size = new System.Drawing.Size(100, 20);
+            this.ID_Append.TabIndex = 2;
+            // 
+            // Amount_Append
+            // 
+            this.Amount_Append.Location = new System.Drawing.Point(93, 59);
+            this.Amount_Append.Name = "Amount_Append";
+            this.Amount_Append.Size = new System.Drawing.Size(100, 20);
+            this.Amount_Append.TabIndex = 3;
+            // 
+            // Add
+            // 
+            this.Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add.Location = new System.Drawing.Point(102, 95);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 4;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // New_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Amount_Append);
+            this.Controls.Add(this.ID_Append);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "New_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Create new object";
+            this.Load += new System.EventHandler(this.New_Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ID_Append;
+        private System.Windows.Forms.TextBox Amount_Append;
+        private System.Windows.Forms.Button Add;
+    }
+}
---
+++ InterfaceInfoSelect/InterfaceInfoSelect/InterfaceInfoSelect/New_Form.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace InterfaceInfoSelect
+{
+    public partial class New_Form : Form
+    {
+        
+        public string pathTxt = @"D:\InterfaceInfoSelect\InterfaceInfoSelect\InterfaceInfoSelect\models.txt";
+
+
+        public New_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void New_Form_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private string MyDictionaryToJson(Dictionary<int, int> dict)
+        {
+            var entries = dict.Select(d =>
+                string.Format("\"{0}\": [{1}]", d.Key, string.Join(",", d.Value)));
+            return "{" + string.Join(",", entries) + "}";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        public string ConvertToString()
+        {
+            string s = string.Empty;
+
+            return s;
+        }
+
+        public void FileChecker()
+        {
+            foreach (var line in File.ReadAllLines(pathTxt))
+            {
+                char[] spliter = {'{', '\"',':', ' ', '[',']','}' };
+
+
+                string[] str = line.Split(spliter, StringSplitOptions.RemoveEmptyEntries);
+
+                int a = int.Parse(str[0]);
+                int b = int.Parse(str[1]);
+
+                _Collect.Add(a, b);
+
+            }
+
+            _counter++;
+        }
+        
+        
+        public Dictionary<int, int> _Collect = new Dictionary<int, int>();
+
+        public void CheckMyDictionary(int id)
+        {
+            // count if some key are insde of the file twice!
+            int counter = 0;
+
+
+            //Foreach every line in Dictionary
+            foreach (var key in _Collect.Keys)
+            {
+
+                //Check if key are true or same key are twice in the file.
+                if (key == id)
+                {
+                    isHere = true;
+                    insdeID = key;
+                    counter++;
+
+
+                    if (counter > 1)
+                    {
+                        _Collect.Remove(key);
+                    }
+                }
+            }
+        }
+
+        public bool isHere = false;
+
+        public int insdeID = 0;
+
+        public int _counter = 0;
+
+        
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int ID = int.Parse(ID_Append.Text);
+                int Amount = int.Parse(Amount_Append.Text);
+
+                //Fill up the dict!
+                if (_counter == 0)
+                {
+                    FileChecker();
+                }
+               
+
+                //Check if exist!
+                 CheckMyDictionary(ID);
+
+                if (isHere)
+                {
+                    _Collect.Remove(insdeID);
+                    _Collect.Add(ID, Amount);
+                }
+                else
+                {
+                    _Collect.Add(ID, Amount);
+                }
+
+
+                OverWriteTheFile();
+               
+                
+               
+                this.Close();
+            }
+            catch
+            {
+
+                MessageBox.Show("Please,Enter a value!");
+            }
+        }
+
+        public void OverWriteTheFile()
+        {
+            string newLine = Environment.NewLine;
+            foreach (var item in _Collect)
+            {
+                File.WriteAllText(pathTxt, MyDictionaryToJson(item.) + newLine);
+            }
+        }
+
+        public void AppendObjectToFile()
+        {
+            File.AppendAllText(pathTxt, MyDictionaryToJson(_Collect));
+        }
+    }
+}
