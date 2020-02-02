7ba9b3923b6c73149017caeee9ccb276dccf6ea3
Nikolai <xxthemineproxx@gmail.com>
2020-02-02 19:43:13 +0200

    newUpdate

--- SoftUniExercises/C# Advanced/CSharp-Advanced-Stacks-Queues-LAB/Lab/08/HotPotato.cs
+++ SoftUniExercises/C# Advanced/CSharp-Advanced-Stacks-Queues-LAB/Lab/08/HotPotato.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 
-
-namespace _07
+namespace _8._Traffic_Jam
 {
-    class HotPotato
+    class Program
     {
         static void Main(string[] args)
         {
+            var cars = new Queue<string>();
 
+            int toPass = int.Parse(Console.ReadLine());
+            int passedCars = 0;
+
+            string command = Console.ReadLine();
+
+            while (command.ToLower() != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < toPass; i++)
+                    {
+                        if (cars.Any())
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            passedCars++;
+                        }
+                    }
+                }
+                else cars.Enqueue(command);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
         }
     }
 }
