2b752792767a2f38f59c24b3b8388b5f099c572e
Nikolai <nikolaistoilkov98@gmail.com>
2020-05-12 15:52:27 +0300

    ANTI-AFK

---
+++ AntiAFK/Anti-AFK/Anti-AFK/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Anti_AFK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Anti_AFK.Properties.Resources.pngtree_c4d_neon_cool_background_image_205279;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(15, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(323, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go Away";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label1.Location = new System.Drawing.Point(46, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Click to be AFK";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.BackgroundImage = global::Anti_AFK.Properties.Resources.pngtree_c4d_neon_cool_background_image_205279;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button2.Location = new System.Drawing.Point(12, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(326, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "I\'m here!";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label3.Location = new System.Drawing.Point(28, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(301, 76);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "00:00:00";
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::Anti_AFK.Properties.Resources.pngtree_c4d_neon_cool_background_image_205279;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(256, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "About";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SPACE",
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AccessibleName = "ANTI-AFK";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Anti_AFK.Properties.Resources.pngtree_c4d_neon_cool_background_image_205279;
+            this.ClientSize = new System.Drawing.Size(350, 292);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ANTI-AFK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Layout += new System.Windows.Forms.LayoutEventHandler(this.Form1_Layout);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ AntiAFK/Anti-AFK/Anti-AFK/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using Hotkeys;
+using WindowsInput;
+using WindowsInput.Native;
+
+
+namespace Anti_AFK
+{
+    public partial class Form1 : Form
+    {
+        private bool click = false;
+        private GlobalHotkey globalHotkey;
+        private InputSimulator inputSimulator;
+        private InputSimulator key;
+        private double hour = 0.0;
+        private double minutes = 0.0;
+        private int _ticks;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.BackColor = System.Drawing.Color.Red;
+            globalHotkey = new GlobalHotkey(Constants.SHIFT, Keys.Up, this);
+            inputSimulator = new InputSimulator();
+            key = new InputSimulator();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            key.Keyboard.KeyPress(VirtualKeyCode.SHIFT);
+            key.Keyboard.KeyPress(VirtualKeyCode.UP);
+
+            click = false;
+            if (!click)
+            {
+
+                button1.BackColor = System.Drawing.Color.Red;
+                button2.BackColor = System.Drawing.Color.Green;
+
+                _ticks = 0;
+                hour = 0;
+                minutes = 0;
+                timer1.Start();
+            }
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            
+
+            click = true;
+            if (click)
+            {
+                button1.BackColor = System.Drawing.Color.Green;
+                button2.BackColor = System.Drawing.Color.Red;
+                timer1.Stop();
+                _ticks = 0;
+                this.label3.Text = "00:00:00";
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = _ticks.ToString();
+
+            //System.Threading.Thread.Sleep(1000);
+            timer1.Interval = 1000;
+            _ticks++;
+
+            ConfigureIncreasement();
+
+            MoveForwards();
+
+            PrintIncreasementTime();
+        }
+        private void MoveForwards()
+        {
+            if (_ticks == 30 || _ticks == 59)
+            {
+                HandleHotKey();
+            }
+        }
+        private void PrintIncreasementTime()
+        {
+            string hour = ShowHours();
+            string minutes = ShowMinutes();
+            string seconds = ShowSeconds();
+
+            this.label3.Text = $"{hour}:{minutes}:{seconds}";
+        }
+
+        private VirtualKeyCode SpecificButton()
+        {
+            switch (comboBox1.Text)
+            {
+                case "A":
+                    return VirtualKeyCode.VK_A;
+                case "B":
+                    return VirtualKeyCode.VK_B;
+                case "C":
+                    return VirtualKeyCode.VK_C;
+                case "D":
+                    return VirtualKeyCode.VK_D;
+                case "E":
+                    return VirtualKeyCode.VK_E;
+                case "F":
+                    return VirtualKeyCode.VK_F;
+                case "G":
+                    return VirtualKeyCode.VK_G;
+                case "H":
+                    return VirtualKeyCode.VK_H;
+                case "I":
+                    return VirtualKeyCode.VK_I;
+                case "J":
+                    return VirtualKeyCode.VK_J;
+                case "K":
+                    return VirtualKeyCode.VK_K;
+                case "L":
+                    return VirtualKeyCode.VK_L;
+                case "M":
+                    return VirtualKeyCode.VK_M;
+                case "N":
+                    return VirtualKeyCode.VK_N;
+                case "O":
+                    return VirtualKeyCode.VK_O;
+                case "P":
+                    return VirtualKeyCode.VK_P;
+                case "Q":
+                    return VirtualKeyCode.VK_Q;
+                case "R":
+                    return VirtualKeyCode.VK_R;
+                case "S":
+                    return VirtualKeyCode.VK_S;
+                case "T":
+                    return VirtualKeyCode.VK_T;
+                case "U":
+                    return VirtualKeyCode.VK_U;
+                case "V":
+                    return VirtualKeyCode.VK_V;
+                case "W":
+                    return VirtualKeyCode.VK_W;
+                case "X":
+                    return VirtualKeyCode.VK_X;
+                case "Y":
+                    return VirtualKeyCode.VK_Y;
+                case "Z":
+                    return VirtualKeyCode.VK_Z;
+                case "0":
+                    return VirtualKeyCode.VK_0;
+                case "1":
+                    return VirtualKeyCode.VK_1;
+                case "2":
+                    return VirtualKeyCode.VK_2;
+                case "3":
+                    return VirtualKeyCode.VK_3;
+                case "4":
+                    return VirtualKeyCode.VK_4;
+                case "5":
+                    return VirtualKeyCode.VK_5;
+                case "6":
+                    return VirtualKeyCode.VK_6;
+                case "7":
+                    return VirtualKeyCode.VK_7;
+                case "8":
+                    return VirtualKeyCode.VK_8;
+                case "9":
+                    return VirtualKeyCode.VK_9;
+                default:
+                    return VirtualKeyCode.SPACE;
+            }
+
+        }
+
+        private void HandleHotKey() => inputSimulator.Keyboard.KeyPress(SpecificButton());
+
+        
+        #region Timer
+        private string ShowHours()
+        {
+            if (hour == 0 && hour <= 9)
+            {
+                return this.label3.Text = $"0{hour}";
+            }
+
+            return this.label3.Text = $"{hour}";
+        }
+        private string ShowMinutes()
+        {
+            if (minutes == 0 || minutes <= 9)
+            {
+                return this.label3.Text = $"0{minutes}";
+            }
+
+            return this.label3.Text = $"{minutes}";
+        }
+        private string ShowSeconds()
+        {
+            if (_ticks == 0 || _ticks <= 9)
+            {
+                return this.label3.Text = $"0{_ticks}";
+            }
+
+            return this.label3.Text = $"{_ticks}";
+        }
+        #endregion
+        private void ConfigureIncreasement()
+        {
+            if (_ticks == 60)
+            {
+                minutes++;
+                _ticks = 0;
+            }
+            else if (minutes == 60)
+            {
+                hour++;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is made by Nikolai Stoilkov.\r\n\r\n\r\n The program press automatically specific key while you are AFK.");
+
+        }
+        #region Empty Methods
+        private void Form1_Layout(object sender, LayoutEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+       
+    }
+}
