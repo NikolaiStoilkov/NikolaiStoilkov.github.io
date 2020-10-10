1e6a8e6b8ba6bddeec79d4b5ca53c87330769112
Nikolai Stoilkov <nikolaistoilkov98@gmail.com>
2020-10-10 15:31:42 +0300

    DOM

--- SoftUniExercises/Javascript-advanced/P04.DOM.LAB/P01. Articles List/solution.js
+++ SoftUniExercises/Javascript-advanced/P04.DOM.LAB/P01. Articles List/solution.js
@@ -1,4 +1,17 @@
 function createArticle() {
-	
-	//TODO...
+	let titleValue = document.getElementById('createTitle').value;
+	let articleContent = document.getElementById('createContent').value;
+
+	// child element
+    let createArticleElement = document.createElement("article");
+
+    // element to append
+    let articleScope = `<h3>${titleValue}</h3><p>${articleContent}</p>`;
+
+    //convert to DOM element
+    createArticleElement.innerHTML = articleScope;
+
+     //return the DOM element to target ID
+     return document.getElementById('articles').appendChild(createArticleElement);
 }
+
